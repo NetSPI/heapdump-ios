b3d2aa6163f41683f5777940cf1b3fc6bbe6c7c1
pacmandu <pacmandu@gmail.com>
2014-11-03 14:50:28 -0600

    Updated heapdump.sh

--- heapdump.sh
+++ heapdump.sh
@@ -9,16 +9,13 @@ if [ $version -ge 1708 ]
 		PID=`ps -ax | grep "$1" | grep -v grep | grep "app" | awk '{print $1}'`
 		echo "App Pid: $PID"
 		echo "GDB Version: $version"
-		#echo "Deleting gdbcmd.txt"
 		rm gdbcmd.txt &>> /dev/null
-		#echo "Deleting dump files"
 		echo ""
 		echo "info mach-regions" >> tmp.txt
 		gdb --pid="$PID" --batch --command=tmp.txt 2>/dev/null | grep sub-regions | awk '{print $3,$5}' | while read range; do
-		echo -n "mach-regions: "
-		echo "$range"
-		cmd="dump binary memory dump`echo $range| cut -d' ' -f1`.dmp $range"
-		echo "$cmd" >> gdbcmd.txt
+			echo "mach-regions: $range"
+			cmd="dump binary memory dump`echo $range| cut -d' ' -f1`.dmp $range"
+			echo "$cmd" >> gdbcmd.txt
 		done
 		rm tmp.txt
 		echo ""
